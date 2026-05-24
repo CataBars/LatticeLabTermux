3d3c1052507066ff94df58c1c3ad31f22357ac31
Mr Krot <vkorotanec@gmail.com>
2026-05-24 14:49:02 +0200

    Changed all the strings in `GlobalStrigns.h` to `global_strings`.

--- GUI/interface/panels/settings/SettingsPanel.cpp
+++ GUI/interface/panels/settings/SettingsPanel.cpp
@@ -8,6 +8,7 @@
 #include "App/AppSignals.h"
 #include "App/UserSettings.h"
 #include "App/capture/CaptureController.h"
+#include "App/language_strings/GlobalStrings.h"
 #include "Engine/Simulation.h"
 #include "GUI/interface/file_dialog/FileDialogManager.h"
 #include "GUI/interface/style/ComboStyle.h"
@@ -18,53 +19,54 @@ namespace {
     const char* integratorName(Integrator::Scheme scheme) {
         switch (scheme) {
         case Integrator::Scheme::Verlet:
-            return "Velocity Verlet";
+            return global_strings.integrator_velocity_verlet.c_str();
         case Integrator::Scheme::KDK:
-            return "KDK";
+            return global_strings.integrator_kdk.c_str();
         case Integrator::Scheme::RK4:
-            return "Runge-Kutta 4";
+            return global_strings.integrator_runge_kutta_4.c_str();
         case Integrator::Scheme::Langevin:
-            return "Langevin";
+            return global_strings.integrator_langevin.c_str();
         }
-        return "Unknown";
+
+        return global_strings.integrator_unknown.c_str();
     }
 
     const char* speedColorModeName(IRenderer::SpeedColorMode mode) {
         switch (mode) {
         case IRenderer::SpeedColorMode::AtomColor:
-            return "Обычная раскраска";
+            return global_strings.speed_color_normal_coloring.c_str();
         case IRenderer::SpeedColorMode::GradientClassic:
-            return "Градиент скорости";
+            return global_strings.speed_color_gradient_coloring.c_str();
         case IRenderer::SpeedColorMode::GradientTurbo:
-            return "Турбо градиент";
+            return global_strings.speed_color_turbo_coloring.c_str();
         }
-        return "Обычная раскраска";
+        return global_strings.speed_color_normal_coloring.c_str();
     }
 
     const char* capturePresetName(CaptureSettings::Preset preset) {
         switch (preset) {
         case CaptureSettings::Preset::Ultrafast:
-            return "ultrafast";
+            return global_strings.capture_preset_ultrafast.c_str();
         case CaptureSettings::Preset::Veryfast:
-            return "veryfast";
+            return global_strings.capture_preset_veryfast.c_str();
         case CaptureSettings::Preset::Faster:
-            return "faster";
+            return global_strings.capture_preset_faster.c_str();
         case CaptureSettings::Preset::Fast:
-            return "fast";
+            return global_strings.capture_preset_fast.c_str();
         case CaptureSettings::Preset::Medium:
-            return "medium";
+            return global_strings.capture_preset_medium.c_str();
         }
-        return "veryfast";
+        return global_strings.capture_preset_veryfast.c_str();
     }
 
     const char* capturePixelFormatName(CaptureSettings::PixelFormat pixelFormat) {
         switch (pixelFormat) {
         case CaptureSettings::PixelFormat::Yuv420p:
-            return "I420";
+            return global_strings.capture_pixel_format_Yuv420p.c_str();
         case CaptureSettings::PixelFormat::Yuv444p:
-            return "I444";
+            return global_strings.capture_pixel_format_Yuv444p.c_str();
         }
-        return "I444";
+        return global_strings.capture_pixel_format_Yuv444p.c_str();
     }
 }
 
@@ -86,31 +88,30 @@ void SettingsPanel::draw(float uiScale, Vec2i windowSize, Simulation& simulation
 
     ImGui::SetNextWindowPos(ImVec2(x, topOffset));
     ImGui::SetNextWindowSize(ImVec2(panelWidth, panelHeight));
-    ImGui::Begin("##SettingsPanel", nullptr, PANEL_FLAGS);
+    ImGui::Begin(global_strings.imgui_settings_panel.c_str(), nullptr, PANEL_FLAGS);
 
-    ImGui::SeparatorText("Симуляция");
+    ImGui::SeparatorText(global_strings.imgui_simulation.c_str());
 
-    ImGui::TextUnformatted("Гравитация");
+    ImGui::TextUnformatted(global_strings.imgui_gravity.c_str());
     ImGui::SameLine();
     Vec3f gravity = simulation.getGravity();
-    if (ImGui::Button("Reset##gravity", ImVec2(50.f * uiScale, 0.f))) {
+    if (ImGui::Button(global_strings.imgui_reset_gravity.c_str(), ImVec2(50.f * uiScale, 0.f))) {
         simulation.setGravity(Vec3f(0, 0, 0));
         gravity = simulation.getGravity();
     }
-
     float gx = gravity.x;
     float gy = gravity.y;
     float gz = gravity.z;
     bool gravityChanged = false;
-    gravityChanged |= ImGui::SliderFloat("Gravity X##gravity_x", &gx, -10.0f, 10.0f, "%.2f");
-    gravityChanged |= ImGui::SliderFloat("Gravity Y##gravity_y", &gy, -10.0f, 10.0f, "%.2f");
-    gravityChanged |= ImGui::SliderFloat("Gravity Z##gravity_z", &gz, -10.0f, 10.0f, "%.2f");
+    gravityChanged |= ImGui::SliderFloat(global_strings.imgui_gravity_x.c_str(), &gx, -10.0f, 10.0f, "%.2f");
+    gravityChanged |= ImGui::SliderFloat(global_strings.imgui_gravity_y.c_str(), &gy, -10.0f, 10.0f, "%.2f");
+    gravityChanged |= ImGui::SliderFloat(global_strings.imgui_gravity_z.c_str(), &gz, -10.0f, 10.0f, "%.2f");
     if (gravityChanged) {
         simulation.setGravity(Vec3f(gx, gy, gz));
     }
 
     Integrator::Scheme currentIntegrator = simulation.getIntegrator();
-    if (ComboStyle::beginCombo("Integrator", integratorName(currentIntegrator), 0.0f, uiScale)) {
+    if (ComboStyle::beginCombo(global_strings.imgui_integrator.c_str(), integratorName(currentIntegrator), 0.0f, uiScale)) {
         const Integrator::Scheme schemes[] = {
             Integrator::Scheme::Verlet,
             Integrator::Scheme::KDK,
@@ -133,53 +134,55 @@ void SettingsPanel::draw(float uiScale, Vec2i windowSize, Simulation& simulation
 
     if (currentIntegrator == Integrator::Scheme::RK4 || currentIntegrator == Integrator::Scheme::Langevin) {
         ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.00f, 0.75f, 0.25f, 1.00f));
-        ImGui::TextWrapped("Внимание: %s пока не реализован и временно работает как Velocity Verlet.", integratorName(currentIntegrator));
+        ImGui::TextWrapped(global_strings.imgui_warning_not_implemented_used_as_velocity_verlet.c_str(), integratorName(currentIntegrator));
         ImGui::PopStyleColor();
     }
 
     float maxParticleSpeed = simulation.getMaxParticleSpeed();
     ImGui::PushItemWidth(150.0f * uiScale);
-    if (ImGui::SliderFloat("Скорость света", &maxParticleSpeed, 0.0f, 100.0f, maxParticleSpeed <= 0.0f ? "не ограничена" : "%.2f")) {
+    if (ImGui::SliderFloat(global_strings.imgui_speed_of_light.c_str(), &maxParticleSpeed, 0.0f, 100.0f,
+                           maxParticleSpeed <= 0.0f ? global_strings.imgui_speed_of_light_unlimited.c_str() : "%.2f")) {
         simulation.setMaxParticleSpeed(maxParticleSpeed);
     }
     ImGui::PopItemWidth();
 
     float accelDamping = simulation.getAccelDamping();
     ImGui::PushItemWidth(150.0f * uiScale);
-    if (ImGui::SliderFloat("Accel damping", &accelDamping, 0.0f, 1.0f, "%.3f")) {
+    if (ImGui::SliderFloat(global_strings.imgui_accel_damping.c_str(), &accelDamping, 0.0f, 1.0f, "%.3f")) {
         simulation.setAccelDamping(accelDamping);
     }
     ImGui::PopItemWidth();
 
     float dt = simulation.getDt();
     ImGui::PushItemWidth(150.0f * uiScale);
-    if (ImGui::SliderFloat("Time step (Dt)", &dt, 0.0001f, 0.05f, "%.4f", ImGuiSliderFlags_AlwaysClamp | ImGuiSliderFlags_Logarithmic)) {
+    if (ImGui::SliderFloat(global_strings.imgui_time_step.c_str(), &dt, 0.0001f, 0.05f, "%.4f",
+                           ImGuiSliderFlags_AlwaysClamp | ImGuiSliderFlags_Logarithmic)) {
         simulation.setDt(dt);
     }
     ImGui::PopItemWidth();
 
     bool bondFormationEnabled = simulation.isBondFormationEnabled();
-    if (ImGui::Checkbox("Образовывать связи", &bondFormationEnabled)) {
+    if (ImGui::Checkbox(global_strings.imgui_bond_formation.c_str(), &bondFormationEnabled)) {
         simulation.setBondFormationEnabled(bondFormationEnabled);
     }
 
     bool ljEnabled = simulation.isLJEnabled();
-    if (ImGui::Checkbox("LJ", &ljEnabled)) {
+    if (ImGui::Checkbox(global_strings.imgui_lj.c_str(), &ljEnabled)) {
         simulation.setLJEnabled(ljEnabled);
     }
     ImGui::SameLine();
     bool coulombEnabled = simulation.isCoulombEnabled();
-    if (ImGui::Checkbox("Coulomb", &coulombEnabled)) {
+    if (ImGui::Checkbox(global_strings.imgui_coulomb.c_str(), &coulombEnabled)) {
         simulation.setCoulombEnabled(coulombEnabled);
     }
 
-    ImGui::SeparatorText("Рендер");
-    ImGui::Checkbox("Сетка", &renderer->drawGrid);
-    ImGui::Checkbox("Связи", &renderer->drawBonds);
+    ImGui::SeparatorText(global_strings.imgui_render.c_str());
+    ImGui::Checkbox(global_strings.imgui_grid.c_str(), &renderer->drawGrid);
+    ImGui::Checkbox(global_strings.imgui_connections.c_str(), &renderer->drawBonds);
 
-    ImGui::TextUnformatted("Цветовая схема");
+    ImGui::TextUnformatted(global_strings.imgui_color_scheme.c_str());
     IRenderer::SpeedColorMode speedMode = renderer->speedColorMode;
-    if (ComboStyle::beginCombo("##speed_color_mode", speedColorModeName(speedMode), 220.0f * uiScale, uiScale,
+    if (ComboStyle::beginCombo(global_strings.imgui_speed_color_mode.c_str(), speedColorModeName(speedMode), 220.0f * uiScale, uiScale,
                                ImGuiComboFlags_HeightLargest)) {
         const IRenderer::SpeedColorMode modes[] = {
             IRenderer::SpeedColorMode::AtomColor,
@@ -201,7 +204,7 @@ void SettingsPanel::draw(float uiScale, Vec2i windowSize, Simulation& simulation
 
     renderer->speedColorMode = speedMode;
 
-    ImGui::TextUnformatted("Макс. скорость градиента");
+    ImGui::TextUnformatted(global_strings.imgui_max_gradien_velocity.c_str());
 
     static float manualSpeedGradientMax = 5.0f;
     bool autoSpeedGradient = renderer->speedGradientMax <= 0.0f;
@@ -212,60 +215,61 @@ void SettingsPanel::draw(float uiScale, Vec2i windowSize, Simulation& simulation
 
     ImGui::PushItemWidth(180.0f * uiScale);
     ImGui::BeginDisabled(autoSpeedGradient || !gradientModeEnabled);
-    if (ImGui::SliderFloat("##speed_gradient_max", &manualSpeedGradientMax, 0.1f, 10.0f, "%.2f")) {
+    if (ImGui::SliderFloat(global_strings.imgui_speed_gradient_max_slider.c_str(), &manualSpeedGradientMax, 0.1f, 10.0f, "%.2f")) {
         renderer->speedGradientMax = manualSpeedGradientMax;
     }
     ImGui::EndDisabled();
     ImGui::SameLine();
     ImGui::BeginDisabled(!gradientModeEnabled);
-    if (ImGui::Checkbox("Авто##speed_gradient_auto", &autoSpeedGradient)) {
+    if (ImGui::Checkbox(global_strings.imgui_auto_speed_gradien.c_str(), &autoSpeedGradient)) {
         renderer->speedGradientMax = autoSpeedGradient ? 0.0f : manualSpeedGradientMax;
     }
     ImGui::EndDisabled();
     ImGui::PopItemWidth();
 
-    ImGui::SeparatorText("Список соседей");
+    ImGui::SeparatorText(global_strings.imgui_neighbour_list.c_str());
     int cellSize = simulation.box().grid.cellSize;
-    if (ImGui::SliderInt("Cell size", &cellSize, 1, 32)) {
+    if (ImGui::SliderInt(global_strings.imgui_cell_size.c_str(), &cellSize, 1, 32)) {
         simulation.setSizeBox(simulation.box().size, cellSize);
     }
 
     float cutoff = simulation.getNeighborListCutoff();
-    if (ImGui::SliderFloat("Cutoff NL", &cutoff, 0.5f, 20.0f, "%.2f")) {
+    if (ImGui::SliderFloat(global_strings.imgui_cutoff_nl.c_str(), &cutoff, 0.5f, 20.0f, "%.2f")) {
         simulation.setNeighborListCutoff(cutoff);
     }
 
     float skin = simulation.getNeighborListSkin();
-    if (ImGui::SliderFloat("Skin NL", &skin, 0.1f, 10.0f, "%.2f")) {
+    if (ImGui::SliderFloat(global_strings.imgui_skin_nl.c_str(), &skin, 0.1f, 10.0f, "%.2f")) {
         simulation.setNeighborListSkin(skin);
     }
 
     if (captureController.isAvailable()) {
-        ImGui::SeparatorText("Запись");
+        ImGui::SeparatorText(global_strings.imgui_write.c_str());
         CaptureSettings captureSettings = captureController.settings();
         const bool recordingActive = captureController.isRecording();
         const std::string captureDir = captureController.outputDirectory().string();
 
-        ImGui::TextUnformatted("Папка сохранения видео");
+        ImGui::TextUnformatted(global_strings.imgui_video_saving_folder.c_str());
         std::array<char, 512> captureDirBuffer{};
         std::snprintf(captureDirBuffer.data(), captureDirBuffer.size(), "%s", captureDir.data());
         const float browseButtonWidth = ImGui::GetFrameHeight();
         ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x - browseButtonWidth - ImGui::GetStyle().ItemSpacing.x);
-        ImGui::InputText("##capture_dir", captureDirBuffer.data(), captureDirBuffer.size(), ImGuiInputTextFlags_ReadOnly);
+        ImGui::InputText(global_strings.imgui_capture_dir.c_str(), captureDirBuffer.data(), captureDirBuffer.size(),
+                         ImGuiInputTextFlags_ReadOnly);
         ImGui::SameLine();
-        if (ImGui::Button("...##capture_dir_browse", ImVec2(browseButtonWidth, 0.0f))) {
+        if (ImGui::Button(global_strings.imgui_capture_dir_browse.c_str(), ImVec2(browseButtonWidth, 0.0f))) {
             fileDialog.openCaptureDirectory(captureDir);
         }
 
         ImGui::BeginDisabled(recordingActive);
 
-        if (ImGui::BeginTable("##capture_settings_table", 2, ImGuiTableFlags_SizingStretchSame)) {
+        if (ImGui::BeginTable(global_strings.imgui_capture_settings_table.c_str(), 2, ImGuiTableFlags_SizingStretchSame)) {
             ImGui::TableNextRow();
 
             ImGui::TableSetColumnIndex(0);
             ImGui::SetNextItemWidth(-FLT_MIN);
             int captureFps = captureSettings.fps;
-            if (ImGui::SliderInt("FPS##capture_fps", &captureFps, 10, 60)) {
+            if (ImGui::SliderInt(global_strings.imgui_fps_capture.c_str(), &captureFps, 10, 60)) {
                 captureSettings.fps = captureFps;
                 captureController.setSettings(captureSettings);
             }
@@ -273,7 +277,7 @@ void SettingsPanel::draw(float uiScale, Vec2i windowSize, Simulation& simulation
             ImGui::TableSetColumnIndex(1);
             ImGui::SetNextItemWidth(-FLT_MIN);
             int crf = captureSettings.crf;
-            if (ImGui::SliderInt("CRF##capture_crf", &crf, 12, 30)) {
+            if (ImGui::SliderInt(global_strings.imgui_crf_capture.c_str(), &crf, 12, 30)) {
                 captureSettings.crf = crf;
                 captureController.setSettings(captureSettings);
             }
@@ -282,7 +286,7 @@ void SettingsPanel::draw(float uiScale, Vec2i windowSize, Simulation& simulation
 
             ImGui::TableSetColumnIndex(0);
             CaptureSettings::Preset preset = captureSettings.preset;
-            if (ComboStyle::beginCombo("Preset##capture_preset", capturePresetName(preset), -FLT_MIN, uiScale)) {
+            if (ComboStyle::beginCombo(global_strings.imgui_preset_capture.c_str(), capturePresetName(preset), -FLT_MIN, uiScale)) {
                 const CaptureSettings::Preset presets[] = {
                     CaptureSettings::Preset::Ultrafast, CaptureSettings::Preset::Veryfast, CaptureSettings::Preset::Faster,
                     CaptureSettings::Preset::Fast,      CaptureSettings::Preset::Medium,
@@ -304,7 +308,8 @@ void SettingsPanel::draw(float uiScale, Vec2i windowSize, Simulation& simulation
 
             ImGui::TableSetColumnIndex(1);
             CaptureSettings::PixelFormat pixelFormat = captureSettings.pixelFormat;
-            if (ComboStyle::beginCombo("Цвет##capture_color", capturePixelFormatName(pixelFormat), -FLT_MIN, uiScale)) {
+            if (ComboStyle::beginCombo(global_strings.imgui_color_capture.c_str(), capturePixelFormatName(pixelFormat), -FLT_MIN,
+                                       uiScale)) {
                 const CaptureSettings::PixelFormat pixelFormats[] = {
                     CaptureSettings::PixelFormat::Yuv444p,
                     CaptureSettings::PixelFormat::Yuv420p,
@@ -330,7 +335,7 @@ void SettingsPanel::draw(float uiScale, Vec2i windowSize, Simulation& simulation
         ImGui::EndDisabled();
     }
 
-    if (ImGui::Button("Reset settings", ImVec2(ImGui::GetContentRegionAvail().x, 0.0f))) {
+    if (ImGui::Button(global_strings.imgui_reset_settings.c_str(), ImVec2(ImGui::GetContentRegionAvail().x, 0.0f))) {
         const UserSettings defaults;
         captureController.setOutputDirectory(defaults.captureOutputDirectory);
         captureController.setSettings(defaults.captureSettings);
@@ -347,8 +352,8 @@ void SettingsPanel::draw(float uiScale, Vec2i windowSize, Simulation& simulation
     }
 
     const float exitButtonWidth = ImGui::GetContentRegionAvail().x;
-    const char* versionText = "LatticeLab v" LATTICELAB_VERSION_STRING " demo";
-    const float versionWidth = ImGui::CalcTextSize(versionText).x;
+    const std::string versionText = global_strings.version_text_pre + LATTICELAB_VERSION_STRING + global_strings.version_text_after;
+    const float versionWidth = ImGui::CalcTextSize(versionText.c_str()).x;
     const float footerHeight = ImGui::GetFrameHeightWithSpacing() + ImGui::GetTextLineHeightWithSpacing();
     const float remaining = ImGui::GetContentRegionAvail().y - footerHeight;
     if (remaining > 0.0f) {
@@ -357,10 +362,10 @@ void SettingsPanel::draw(float uiScale, Vec2i windowSize, Simulation& simulation
 
     ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.72f, 0.76f, 0.80f, 0.85f));
     ImGui::SetCursorPosX(std::max(0.0f, (ImGui::GetContentRegionAvail().x - versionWidth) * 0.5f));
-    ImGui::TextUnformatted(versionText);
+    ImGui::TextUnformatted(versionText.c_str());
     ImGui::PopStyleColor();
 
-    if (ImGui::Button("Exit", ImVec2(exitButtonWidth, 0.0f))) {
+    if (ImGui::Button(global_strings.imgui_exit_button.c_str(), ImVec2(exitButtonWidth, 0.0f))) {
         AppSignals::UI::ExitApplication.emit();
     }
 
