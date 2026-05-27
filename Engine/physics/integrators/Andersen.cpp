f23ca2dae7f97118c29c5b689f26531fe1120ee8
Egor <trusov.egor@gmail.com>
2026-05-27 15:01:08 +0300

    feat: реализация для функции мк шага
    Данная функция иметирует контакт атомов с термостатом.

--- Engine/physics/integrators/Andersen.cpp
+++ Engine/physics/integrators/Andersen.cpp
@@ -16,5 +16,24 @@ void Andersen::pipeline(StepData& stepData)
 
 void Andersen::mkMove(StepData& stepData)
 {
-    
+    AtomStorage& atomStorage = stepData.world.getAtomStorage();
+    double probability = stepData.dt * nu;
+    std::uniform_real_distribution<double> uniDist(0.0, 1.0);
+    size_t N = atomStorage.size(); 
+
+    for(int i = 0; i < N; ++i)
+    {
+        double randomVal = uniDist(randomGenerator);
+
+        if(randomVal < probability)
+        {
+            float m = AtomData::getProps(atomStorage.type(i)).mass;
+            float sigma = std::sqrt(Units::kboltzmann * t / m);
+            std::normal_distribution<float> Maksvell(0.f, sigma);
+
+            atomStorage.velX(i) = Maksvell(randomGenerator);
+            atomStorage.velY(i) = Maksvell(randomGenerator);
+            atomStorage.velZ(i) = Maksvell(randomGenerator);
+        }
+    }
 }
